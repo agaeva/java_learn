086a9ce686cfd7b01fa7a6f37974b3a85ce38850
agaeva <agenalyyy@gmail.com>
2020-10-30 12:37:49 +0300

    вычисление расстояния между двумя точками

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+  public class Point {
+    public double x, y;
+
+    public Point(double x, double y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public String view() {
+      return this.x+ ";" + this.y;
+    }
+
+    public double distance(Point p2) {
+      return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y)*(p2.y - this.y));
+    }
+
+  }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Squere.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class Squere {
+  public double l;
+
+  public Squere(double l) {
+    this.l = l;
+  }
+
+  public  double area () {
+    return this.l * this.l;
+  }
+}
