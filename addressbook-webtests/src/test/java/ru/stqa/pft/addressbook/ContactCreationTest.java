2f7f10361f03e9d39119b646d29e531d5c1d35c2
agaeva <agenalyyy@gmail.com>
2020-11-08 16:19:45 +0300

    test add contact

---
+++ addressbook-webtests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -0,0 +1,98 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ContactCreationTest {
+  private WebDriver wd;
+
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    login();
+    clickButtonAdd();
+    fillFormContact(new DateContact("Ivan", "Petrov", "Tver", "557868686", "938374664", "383664664", "test@test.ru", "r"));
+    addAllContact();
+  }
+
+  private void addAllContact() {
+    wd.findElement(By.xpath("//input[21]")).click();
+  }
+
+  private void fillFormContact(DateContact groupDateContact) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(groupDateContact.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(groupDateContact.getLastname());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(groupDateContact.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(groupDateContact.getPhone1());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(groupDateContact.getPhone2());
+    wd.findElement(By.name("work")).click();
+    wd.findElement(By.name("work")).clear();
+    wd.findElement(By.name("work")).sendKeys(groupDateContact.getPhone3());
+    wd.findElement(By.name("fax")).click();
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(groupDateContact.getEmail());
+    wd.findElement(By.name("address2")).click();
+    wd.findElement(By.name("address2")).clear();
+    wd.findElement(By.name("address2")).sendKeys(groupDateContact.getAddress2());
+  }
+
+  private void clickButtonAdd() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  private void login() {
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    }
+    catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    }
+    catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
---
+++ addressbook-webtests/src/test/java/ru/stqa/pft/addressbook/DateContact.java
@@ -0,0 +1,55 @@
+package ru.stqa.pft.addressbook;
+
+public class DateContact {
+    private final String firstname;
+    private final String lastname;
+    private final String address;
+    private final String phone1;
+    private final String phone2;
+    private final String phone3;
+    private final String email;
+    private final String address2;
+
+    public DateContact(String firstname, String lastname, String address, String phone1, String phone2, String phone3, String email, String address2) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.phone3 = phone3;
+        this.email = email;
+        this.address2 = address2;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public String getPhone3() {
+        return phone3;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+}
